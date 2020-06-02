d5566761744cbd6cbd2387ebc1f990da41a0954a
myadventuresincoding <66219262+myadventuresincoding@users.noreply.github.com>
2020-06-01 22:33:34 -0600

    Add comments to explain CompletableFuture example

--- src/main/java/ItemGateway.java
+++ src/main/java/ItemGateway.java
@@ -9,10 +9,13 @@ public class ItemGateway {
 
     public List<Integer> getItems(List<Integer> items) {
 
+        // Create a CompletableFuture for each item being requested
+        // and start each request asynchronously
         List<CompletableFuture<List<Integer>>> futures = items.stream()
                 .map(item -> CompletableFuture.supplyAsync(() -> getItem(item)))
                 .collect(Collectors.toList());
 
+        // Wait for ALL CompletableFutures to finish then return
         return futures.stream()
                 .map(CompletableFuture::join)
                 .flatMap(List::stream)
@@ -21,6 +24,8 @@ public class ItemGateway {
 
     private List<Integer> getItem(Integer item) {
         try {
+            // Simulate a call to some external resource
+            // by sleeping for a second
             System.out.println("Start: " + item);
             Thread.sleep(1000);
             System.out.println("End: " + item);
